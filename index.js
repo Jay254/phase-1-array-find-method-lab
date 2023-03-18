c4af34cfcebebcfa9ef5a72dadad03795f1ca53f
Jay254 <jyegon504@gmail.com>
2023-03-18 18:22:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record){
+     for(const x of record){
+        if(x.result === "W"){
+            return x.year;
+        }
+        
+     }
+    //return 'N/A';
+    
+}
+
+records.find(superbowlWin);
+
+
+
+
